b7cb1f7f074be1df8a9c634e0949e03138210237
sujang <sujang.kang@gmail.com>
2026-03-25 10:25:03 +0900

    タイマー形式の修正

--- ScoreFrame/ScoreFrame/Models/TimerSegment.swift
+++ ScoreFrame/ScoreFrame/Models/TimerSegment.swift
@@ -8,6 +8,10 @@ struct TimerSegment: Codable, Identifiable, Equatable {
     var timerStopTime: TimeInterval?
     var timerStartOffset: TimeInterval?
 
+    // タイマー表示オプション
+    var showPlusPrefix: Bool = false     // "+MM:SS" 形式で表示
+    var timerColorHex: String? = nil     // タイマー文字色 ("#RRGGBB")
+
     /// ピリオド切替・タイマー表示の起点。segmentStartTime が未設定なら timerStartTime を使う
     var effectiveStartTime: TimeInterval? {
         segmentStartTime ?? timerStartTime
